f8bd49c1f5db0e6418942bffcdba7d0903285734
arthurcezars <arthurcezars@gmail.com>
2022-09-18 11:23:25 -0300

    Criando o primeiro teste automatizado e auterando a propriedade idade para a nova regra

--- codigo/main.py
+++ codigo/main.py
@@ -1,5 +1,11 @@
 from bytebank import Funcionario
 
-lucas = Funcionario("Lucas Carvalho", 2000, 1000)
+# lucas = Funcionario("Lucas Carvalho", '13/03/2000', 1000)
 
-print(lucas.idade())
+# print(lucas.idade())
+
+def teste_idade():
+    funcionario = Funcionario('Teste', '13/03/2000', 1111)
+    print(f'Teste = {funcionario.idade()}')
+
+teste_idade()

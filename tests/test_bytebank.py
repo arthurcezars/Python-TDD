aeb13d7b9d15d609823b565d357a1f430cd7d88a
arthurcezars <arthurcezars@gmail.com>
2022-09-18 14:59:58 -0300

    Adicionado a teste automatizado para o metodo sobrenome, adicionado uma fixture do pytest para criação do objeto funcionario

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -1,13 +1,31 @@
 from codigo.bytebank import Funcionario
+import pytest
 
 
 class TestClass:
-    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+    # Passando o nome do metodo para os testes o pytest faz a criação do objeto no momento de execução dos teste
+    @pytest.fixture(scope='function')
+    def funcionario(self):
+        return Funcionario(' Lucas Carvalho ', '13/03/2000', 1111)
+
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self, funcionario):
         # Given-When-Then
         entrada = '13/03/2000' # Given-Contexto
         esperado = 22
 
-        funcionario_teste = Funcionario('Teste', entrada, 1111)
-        resultado = funcionario_teste.idade() # When-Ação
+        # funcionario_teste = Funcionario('Teste', entrada, 1111)
+        # resultado = funcionario_teste.idade() # When-Ação
+        resultado = funcionario.idade() # When-Ação
+
+        assert esperado == resultado # Then-Desfecho
+
+    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self, funcionario):
+        # Given-When-Then
+        entrada = ' Lucas Carvalho ' # Given-Contexto
+        esperado = 'Carvalho'
+
+        # lucas = Funcionario(entrada, '11/11/2000', 1111)
+        # resultado = lucas.sobrenome() # When-Ação
+        resultado = funcionario.sobrenome() # When-Ação
 
         assert esperado == resultado # Then-Desfecho
